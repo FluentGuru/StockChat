7be9344e9bdbe013b394249c2e66c36faf10ea02
Paulo Tavares <paulo.enmanuel@hotmail.com>
2020-05-03 23:29:51 -0400

    Adding bot project

---
+++ src/Jobsity.StockChat.Bot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Jobsity.StockChat.Bot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
